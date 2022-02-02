28193d1eeaa09436ae763e53726d2a3e95c1f41f
Valdo Romao <v.romao@thecodingmachine.com>
2022-02-02 11:58:07 +0000

    Added dusseldorf map + edits

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,37 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 
-import {bootstrapExtra} from "@workadventure/scripting-api-extra";
-
-// The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
-bootstrapExtra().catch(e => console.error(e));
-
-let currentPopup: any = undefined;
-const today = new Date();
-const time = today.getHours() + ":" + today.getMinutes();
-
-WA.room.onEnterLayer('clockZone').subscribe(() => {
-    console.log('toto')
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
-})
-
-WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
-
-function closePopUp(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
+const zoneList: any = {
+    popupCrackTheCode: {
+        message: 'Congrats, you just found the cartwheels, click to discover the riddle of the lock 3', 
+        buttons: [
+            {
+                label: "It's not for me",
+                className: "popUpElement",
+                callback: (popupValue: any) => {
+                    popupValue.close();
+                }
+            },
+            {
+                label: "Challenge accepted!",
+                className: "popUpElement",
+                callback: (popupValue: any) => {
+                    WA.nav.openCoWebSite('https:\/\/valdotr.github.io\/veepee-dusseldorf-map\/src\/assets\/CROSSWORD-GAME.pdf');
+                    popupValue.close();
+                }
+            },
+        ]
     }
+};
+
+const zoneListArray = Object.keys(zoneList);
+for(let i = 0; i < zoneListArray.length; i++) {
+    const index = zoneListArray[i];
+    let popup: any = null;
+    WA.room.onEnterZone(index, () => {
+        popup = WA.openPopup(`${index}`, zoneList[index].message, zoneList[index].buttons);
+    });
+    WA.room.onLeaveZone(index, () => {
+        popup.close();
+        WA.closeCoWebSite();
+    });
 }
